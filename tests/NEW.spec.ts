dea7cd26df63a5c5fae90fa0b63ed3b3fbfbce29
unknown <karikarim157@gmail.com>
2026-09-07 10:32:51 +0530

    new message

--- tests/NEW.spec.ts
+++ tests/NEW.spec.ts
@@ -1,11 +1,11 @@
 import {request, test, expect} from '@playwright/test';
 
-test.only('example test', async ({ request }) => {
+test.only('example test', async ({ request ,page}) => {
   const response = await request.get('https://playwright.dev/');
   console.log(await response.text());
   await expect.soft(response).toBeOK();
   const response2 = await request.get('https://playwright.dev/docs/intro');
  console.log(await response2.text());
   await expect.soft(response2).toBeOK(); 
-  await page.waitfortimeout(2000);
+  await page.waitForTimeout(2000);
 });
